966375f72bf279f9f6f3f9ee13ff7503da49ae4c
trevor <me@trevorbehnke.com>
2021-11-08 10:45:25 -0800

    feat: :construction_worker: 127

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,15 @@
 import styles from "./Login.module.css";
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -29,7 +31,13 @@ export default function Login() {
           value={password}
         />
       </label>
-      <button className="btn">Login</button>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
